4e218600cb9d29a18c133d0b6da96835d882e79a
Sam Bonning <sbonning@gmail.com>
2017-05-19 19:17:25 +0800

    updated for v.1 - 5.19

--- tikibar.py
+++ tikibar.py
@@ -1,27 +1,16 @@
 ##Sam's Tiki Bar project in the "Pirate Bartender" Assignment for Thinkful
 ##Mixing delicious tiki drinks since 2017!
 ##Description of functionality:
-## - the Bartender will ask a series of questions about your tastes
+## - the Bartender will ask a series of questions about your tastes (mug and drink)
 ## - the Bartender will mix you up a delicious beverage/s until you are done!
-
-def drink_program ():
-	"""Ties the entire program together"""
-	welcome()
-
-	glass_pref()
-
-	drink_pref()
-
-	drink_mix(drink)
-
-	print ("Here you go!")
 
 def welcome ():
 	"""Welcome the customer to the bar"""
-	print ("A bartender with one eye greets you with a smile.  Or a grimace.  It's hard to tell.\n")
+	print ("\n\n\n\nA bartender with one eye greets you with a smile.  Or a grimace.  It's hard to tell.\n")
 	print ("	-Welcome to Sam's Tiki Bar!")
 
-def glass_pref ():
+
+def glass_pref():
 	"""I welcome the customer and choose a glass for the drink"""
 	glassware = {'1': "cute animal mug", '2': "culturally appropriate mug"}
 	print ("	-First choose your glassware.  We pride ourselves on our original Tiki mugs!")
@@ -35,11 +24,11 @@ def glass_pref ():
 		except:
 			print ("	-I've only got novelty mugs. Sorry!")
 			glass = input("If you want one of those cute animals, choose 1.\nFor the cultural option, choose 2.\n")
-	# how do I add a space between the two options?
-	print ("Great choice. I love a {}.".format(glassware.get(glass)))
-	return glass
+	print ("	-Great choice. I love a {}.".format(glassware.get(glass)))
+	pauser = input("Hit any key to continue\n")
+	return glassware.get(glass)
 
-def drink_pref ():
+def drink_pref():
 	"""I gather the input about customer taste preferences"""
 	print ("The bartender waves your mug choice at you.\n")
 	## later I want to add an input for the mug from the glass_pref
@@ -65,10 +54,12 @@ def drink_pref ():
 		except:
 			print ("Sorry to ask again, but I got a bit confused.  Simple 'Yes' or 'No' will do.")
 			repeat = True
-	print ("Ok I got all that!  I'll mix it up!")
+	print ("\n	-Ok I got all that!  I'll mix it up!")
+	pauser = input("Hit any key to continue\n")
 	return drink
 
-def drink_mix (drink):
+
+def drink_mix (drink = {'strong':1, 'salty':1, 'bitter':1, 'fruity': 0}):
 	"""I mix up a random drink based on what the customer likes"""
 	your_drink = []
 	import random
@@ -80,11 +71,49 @@ def drink_mix (drink):
 	}
 	for d in drink.keys():
 		new_ing = random.choice(ingredients[d])
-		your_drink.append(new_ing)
-		print (your_drink)
-	print (your_drink)
-		
+		if drink[d] == 1:
+			your_drink.append(new_ing)
+		else:
+			continue
+	if your_drink == []:
+		print ("Well, it seems that you did something to offend the bartender, because he gave you a water.")
+	else:
+		ing_string = ""
+		if len(your_drink) == 1:
+			ing_string = your_drink[0]
+		else:
+			for a in your_drink[0:len(your_drink)-1]:
+				ing_string = ing_string+a+", "
+			ing_string = ing_string+"and "+ your_drink[len(your_drink)-1]+"."
+		return ing_string
+
+def deliver_drink(ing_string, glass, another):
+	print ("	-Here you go.  I've mixed up a special one for you!")
+	print ("	-In your {0}, highlights of {1}".format(glass,ing_string))
+	print ("	-Enjoy! Let me know if you'd like another.")
+	another = input("Done already? Say yes if you'd like another.")
+	if another.upper() == "YES":
+		another = True
+	else:
+		another = False
+	return another
 
 
 
+
+def drink_program ():
+	"""Ties the entire program together"""
+	welcome()
+	another = True
+	
+	while another == True:
+		glass = glass_pref()
+		drink = drink_pref()
+		ing_string = drink_mix(drink)
+		another = deliver_drink(ing_string,glass,another)
+		## can we please talk about local v global variables here?  I'm not sure if I've done something sticky with another
+	print("    -Thanks for drinking with us!")
+
+if __name__ == "__main__":
+	drink_program()
 
